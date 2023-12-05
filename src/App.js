ae878fae33b4c00f11af90a460f2e0e6a7d00a84
akkologlu <abdullahakkol@gmail.com>
2023-12-05 19:38:20 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import axios from "axios";
 
 function App() {
   const [response, setResponse] = useState("");
-  const openaiApiKey = "API_KEY_HERE";
+  const openaiApiKey = "";
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [industry, setIndustry] = useState("");
+  const [position, setPosition] = useState("");
+  const [technologies, setTechnologies] = useState("");
+  const [background, setBackground] = useState("");
+  const [extra, setExtra] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
 
   const handleOpenAIRequest = async () => {
     try {
@@ -15,19 +27,18 @@ function App() {
           {
             role: "system",
             content:
-              "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+              "You are a cover letter writer who specializes in writing cover letters for candidates who will apply for a job or internship. But do not write it like a regular cover letter. Start the letter directly with dear. Write it without including the previous date, name, address and parts like that.",
           },
           {
             role: "user",
-            content:
-              "Compose a poem that explains the concept of recursion in programming.",
+            content: `Hi GPT! My name is ${name}. I will apply for a position at ${company} company. I will apply for the ${position} position. Technologies I know: ${technologies}. You can mention these. Expreiencer that I have : ${background}. And I have add this as a extra for your knowledge to use them in the letter: ${extra} Write a cover letter using this information.`,
           },
         ],
       };
 
       const headers = {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${openaiApiKey}`,
+        Authorization: openaiApiKey,
       };
 
       const result = await axios.post(endpoint, requestBody, { headers });
@@ -37,9 +48,95 @@ function App() {
     }
   };
 
+  const handleInputChange = (e) => {
+    // Update the state based on the input name
+    switch (e.target.name) {
+      case "name":
+        setName(e.target.value);
+        break;
+      case "company":
+        setCompany(e.target.value);
+        break;
+      case "industry":
+        setIndustry(e.target.value);
+        break;
+      case "position":
+        setPosition(e.target.value);
+        break;
+      case "technologies":
+        setTechnologies(e.target.value);
+        break;
+      case "background":
+        setBackground(e.target.value);
+        break;
+      case "extra":
+        setExtra(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div>
-      <button onClick={handleOpenAIRequest}>Make OpenAI Request</button>
+      <h1>{process.env.REACT_APP_API_KEY}</h1>
+      <div>
+        <form className="flex flex-col" onSubmit={handleSubmit}>
+          <label>Enter your name:</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleInputChange}
+          />
+          <label>Enter the company's name</label>
+          <input
+            type="text"
+            name="company"
+            value={company}
+            onChange={handleInputChange}
+          />
+          <label>Enter the company's industry</label>
+          <input
+            type="text"
+            name="industry"
+            value={industry}
+            onChange={handleInputChange}
+          />
+          <label>Enter the position you are applying for</label>
+          <input
+            type="text"
+            name="position"
+            value={position}
+            onChange={handleInputChange}
+          />
+          <label>Enter your skills</label>
+          <textarea
+            type="text"
+            name="technologies"
+            value={technologies}
+            onChange={handleInputChange}
+          />
+          <label>Enter your previous expreince and explain them in short</label>
+          <textarea
+            type="text"
+            name="background"
+            value={background}
+            onChange={handleInputChange}
+          />
+          <label>Anything you want to add extra</label>
+          <textarea
+            type="text"
+            name="extra"
+            value={extra}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleOpenAIRequest} onSubmit={handleSubmit}>
+            Make OpenAI Request
+          </button>
+        </form>
+      </div>
+
       <p>OpenAI Response: {response}</p>
     </div>
   );
